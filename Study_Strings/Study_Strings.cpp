b8442d4e0d6f9ed0babd7c04cc2104d5160e1f32
AlexanderVinarsky <vinarskydichtv@gmail.com>
2022-07-04 13:35:40 +0300

    Добавьте файлы проекта.

---
+++ Study_Strings/Study_Strings.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    char str[50] = "abcdefghijklmnopqrstuvwxyz";
+    scanf_s("%s", str, 50);
+
+    printf("printf:\n");
+    printf("%s\n\n", str);
+
+    printf("cycle:\n");
+    for (int i = 0; i < 50; i++)
+        printf("%c", str[i]);
+
+    printf("\n\n");
+
+    printf("codes:\n");
+    for (int i = 0; i < 50; i++)
+        printf("%d, ", (int)str[i]);
+
+    printf("\n\n");
+}
